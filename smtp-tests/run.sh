5c769c6f6e794350128713008f89fcb6e01d1d72
Gerhard <gerhard@tinned-software.net>
2026-06-15 10:26:10 +0200

    added helpscreen, version screen, argument parsing for verbose mode

--- smtp-tests/run.sh
+++ smtp-tests/run.sh
@@ -1,16 +1,61 @@
 #!/bin/bash
+#
+VERSION="0.0.5"
+#
+
 
 # Color definition
-# shellcheck disable=SC2034
 GREEN='\e[0;32m'
-# shellcheck disable=SC2034
 YELLOW='\e[0;33m'
-# shellcheck disable=SC2034
 RED='\e[0;31m'
 # shellcheck disable=SC2034
 BLUE='\e[0;36m'
-# shellcheck disable=SC2034
 RST='\e[0m'
+
+#
+# Help screen
+#
+function help_screen () {
+    echo
+    echo "Usage: $(basename "$0") [-hv] [--verbose]"
+    echo "  -h  --help                      print this usage and exit"
+    echo "  -v  --version                   print version information and exit"
+    echo "      --verbose                   Show verbose details like SMTP transaction for failed tests"
+    echo
+}
+
+function version_screen
+{
+    echo
+    echo "$(basename "$0") Version $VERSION"
+    echo
+}
+
+VERBOSE=0
+while [ $# -gt 0 ]; do
+    case $1 in
+        # General parameter
+        -h|--help)
+            help_screen
+            exit 0
+            ;;
+
+        -v|--version)
+            version_screen
+            exit 0
+            ;;
+
+        --verbose)
+            VERBOSE=1
+            shift 1
+            ;;
+        *)
+            errorout "ERROR: Unknown option '$1'."
+            help_screen
+            exit 1
+            ;;
+    esac
+done
 
 if [[ ! -f "./vars.conf" ]]; then
 	echo -e "${RED}Error: vars.conf not found.${RST}"
@@ -26,7 +71,6 @@ bash ./prepare.sh
 echo
 
 # get the list of testcases
-# shellcheck disable=SC2012
 readarray -t TESTCASES < <(find "${CONFIG_PATH}" -name '*.conf' -exec basename {} \; | sort)
 
 # create the log directory if not existing
@@ -35,9 +79,11 @@ if [[ ! -d "${LOG_PATH}" ]]; then
 fi
 
 FAILED_TESTS=0
+TOTAL_TESTS=0
 
 for TC in "${TESTCASES[@]}"; do
 	[[ -z "$TC" ]] && continue
+	TOTAL_TESTS=$((TOTAL_TESTS + 1))
 
 	# Extract expected return code from filename (e.g., swaks__...__0.conf -> 0)
 	if [[ "$TC" =~ __([0-9]+)\.conf$ ]]; then
@@ -71,7 +117,7 @@ for TC in "${TESTCASES[@]}"; do
 	echo -e "Testcase ...... ${COLOR}${RESULT_STR}${RST} ${DOTS} ${TC_NAME}"
 
 	# If test failed, show the full swaks log for debugging
-	if [[ "${TC_RC}" -ne "${TC_RC_EXP}" ]]; then
+	if [[ "${TC_RC}" -ne "${TC_RC_EXP}" ]] && [[ "${VERBOSE}" -eq "1" ]]; then
 		echo -e "${YELLOW}Debug: Full log:${RST}"
 		sed 's/^/  /' "${LOG_PATH}/${TC_NAME}.log"
 		echo
@@ -82,6 +128,6 @@ if [[ ${FAILED_TESTS} -gt 0 ]]; then
 	echo -e "\n${RED}Error: ${FAILED_TESTS} tests failed.${RST}"
 	exit 1
 else
-	echo -e "\n${GREEN}All tests passed successfully.${RST}"
+	echo -e "\n${GREEN}All ${TOTAL_TESTS} tests passed successfully.${RST}"
 	exit 0
 fi
